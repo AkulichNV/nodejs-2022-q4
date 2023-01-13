147e8104262c5bf03fd88e2938a801e33edd8cfb
Natalia <sh.nat.vlad@gmail.com>
2023-01-13 18:13:12 +0600

    feat: add try..catch and handle error 500

--- task-3-crud-api/src/app.ts
+++ task-3-crud-api/src/app.ts
@@ -1,68 +1,78 @@
 const fs = require('fs');
 const url = require('url');
-// const querystring = require('querystring');
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
-// const file = fs.readFileSync(path.resolve(__dirname, "../data/compSciCourses.json"));
+// const file = fs.readFileSync(path.resolve(__dirname, "../data.json"));
 const data = fs.readFileSync(path.resolve(__dirname,'./data.json'));
 let users = JSON.parse(data);
 
-const requestHandler = (req, res) => {
+const requestHandler = (async (req, res) => {
     const method = req.method;
     const urlparse = url.parse(req.url, true);
     const userId = urlparse.pathname.split('/')[3];
-
-    // console.log(urlparse.pathname);
-    // console.log(method);
   
   if(urlparse.pathname === '/api/users' && method === 'GET') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    return res.end(JSON.stringify(users, null, 2));
+    try {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(users, null, 2));
+    } catch (err) { 
+      console.log(`Error in Catch commentOnGet:  ${err}`);
+      const message = { error: 'Something went wrong' };
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(message, null, 2));
+    }
   }
   else if(urlparse.pathname === `/api/users/${userId}` && method === 'GET') {
-    const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
-    if(isId) {
+    try {
+        const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
+      if(isId) {
         const existId = Object.keys(users).find((item) => item === userId);
         if(existId){
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            return res.end(JSON.stringify(users[existId], null, 2));
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          return res.end(JSON.stringify(users[existId], null, 2));
         } else {
-            const message = { message: 'your id doesn\'t exist' };
+          const message = { message: 'your id doesn\'t exist' };
 
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            return res.end(JSON.stringify(message, null, 2));
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          return res.end(JSON.stringify(message, null, 2));
         }
-    } 
+      } 
       const message = { message: 'your id is invalid (not uuid)' };
-
       res.writeHead(400, {'Content-Type': 'application/json'});
       return res.end(JSON.stringify(message, null, 2));
+    } catch (err) { 
+      console.log(`Error in Catch commentOnGet: ${err}`);
+      const message = { error: 'Something went wrong' };
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(message, null, 2));
+    }
   }
   else if(urlparse.pathname === '/api/users' && method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {    
-      body += chunk.toString();
-    });
-    req.on('end', () => {
+    try {
+      let body = '';
+      req.on('data', chunk => {    
+        body += chunk.toString();
+      });
+      req.on('end', () => {
         const id = uuidv4();
 
         const jsonData = JSON.parse(body);
         const username = jsonData.username;
         const age = jsonData.age;
         const hobbies = jsonData.hobbies;
-
+  
         const usernameExist = Object.keys(users).find(key => users[key].username === username);
-
+  
         if (username && age && hobbies) {
           if(usernameExist) {
             const message = { message: 'this username already exist!' };
             res.writeHead(400, {'Content-Type': 'application/json'});
             return res.end(JSON.stringify(message, null, 2));
           } 
-
+  
           users[id] = { username, age, hobbies };
-          fs.writeFile('src/data.json', JSON.stringify(users), (err) => {            
+          fs.writeFile('src/data.json', JSON.stringify(users, null, 2), (err) => {            
             if (err) {
               const message = { message: 'could not persist data!' };
               res.writeHead(400, {'Content-Type': 'application/json'});
@@ -73,84 +83,106 @@ const requestHandler = (req, res) => {
           });
         } else {
           const message = { message: 'request body does not contain required fields' };
-      
+        
           res.writeHead(400, {'Content-Type': 'application/json'});
           return res.end(JSON.stringify(message, null, 2));
         }
       });
+    } catch (err) { 
+      console.log(`Error in Catch commentOnPost: ${err}`);
+
+      const message = { error: 'Something went wrong' };
+
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(message, null, 2));
+     }
   }
   else if(urlparse.pathname === `/api/users/${userId}` && method === 'PUT') {
-    const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
-    if(isId) {
+    try {
+      const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
+      if(isId) {
         const existId = Object.keys(users).find((item) => item === userId);
         if(existId){
-            let body = '';
-
-            req.on('data', chunk => {    
+          let body = '';
+  
+          req.on('data', chunk => {    
             body += chunk.toString();
-            });
+          });
+  
+          req.on('end', () => {
+            const id = uuidv4();
+          
+            const jsonData = JSON.parse(body);
+            const username = jsonData.username;
+            const age = jsonData.age;
+            const hobbies = jsonData.hobbies;
 
-            req.on('end', () => {
-                const id = uuidv4();
-        
-                const jsonData = JSON.parse(body);
-                const username = jsonData.username;
-                const age = jsonData.age;
-                const hobbies = jsonData.hobbies;
+            users[userId] = { username, age, hobbies };
 
-                users[userId] = { username, age, hobbies };
-
-                fs.writeFile('src/data.json', JSON.stringify(users), (err) => {            
-                    if (err) {
-                      const message = { message: 'could not persist data!' };
-                      res.writeHead(400, {'Content-Type': 'application/json'});
-                      return res.end(JSON.stringify(message, null, 2));
-                    } 
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    return res.end(JSON.stringify(users, null, 2));
-                })
+            fs.writeFile('src/data.json', JSON.stringify(users, null, 2), (err) => {            
+              if (err) {
+                const message = { message: 'could not persist data!' };
+                res.writeHead(400, {'Content-Type': 'application/json'});
+                return res.end(JSON.stringify(message, null, 2));
+              } 
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify(users, null, 2));
             })
+          })
         } else {
-            const message = { message: 'your id doesn\'t exist' };
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            return res.end(JSON.stringify(message, null, 2));
+          const message = { message: 'your id doesn\'t exist' };
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          return res.end(JSON.stringify(message, null, 2));
         }
-    } else {
-      const message = { message: 'your id is invalid (not uuid)' };
-      res.writeHead(400, {'Content-Type': 'application/json'});
+      } else {
+        const message = { message: 'your id is invalid (not uuid)' };
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify(message, null, 2));
+      }
+    } catch (err) { 
+      console.log(`Error in Catch commentOnPut: ${err}`);
+      const message = { error: 'Something went wrong' };
+      res.writeHead(500, {'Content-Type': 'application/json'});
       return res.end(JSON.stringify(message, null, 2));
     }
   }
   else if(urlparse.pathname === `/api/users/${userId}` && method === 'DELETE') {
-    const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
-    if(isId) {
+    try {
+      const isId = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId);
+      if(isId) {
         const existId = Object.keys(users).find((item) => item === userId);
         if(existId){
-            delete users[userId];
-            fs.writeFile('src/data.json', JSON.stringify(users), (err) => {            
-                if (err) {
-                  const message = { message: 'could not persist data!' };
-                  res.writeHead(400, {'Content-Type': 'application/json'});
-                  return res.end(JSON.stringify(message, null, 2));
-                } 
-                res.writeHead(204, {'Content-Type': 'application/json'});
-                return res.end();
-            })
+          delete users[userId];
+          fs.writeFile('src/data.json', JSON.stringify(users, null, 2), (err) => {            
+            if (err) {
+              const message = { message: 'could not persist data!' };
+              res.writeHead(400, {'Content-Type': 'application/json'});
+              return res.end(JSON.stringify(message, null, 2));
+            } 
+            res.writeHead(204, {'Content-Type': 'application/json'});
+            return res.end();
+          })
         } else {
-            const message = { message: 'your id doesn\'t exist' };
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            return res.end(JSON.stringify(message, null, 2));
+          const message = { message: 'your id doesn\'t exist' };
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          return res.end(JSON.stringify(message, null, 2));
         }
-    } else {
-      const message = { message: 'your id is invalid (not uuid)' };
-      res.writeHead(400, {'Content-Type': 'application/json'});
+      } else {
+        const message = { message: 'your id is invalid (not uuid)' };
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify(message, null, 2));
+      }
+    } catch (err) { 
+      console.log(`Error in Catch commentOnDelete: ${err}`);
+      const message = { error: 'Something went wrong' };
+      res.writeHead(500, {'Content-Type': 'application/json'});
       return res.end(JSON.stringify(message, null, 2));
     }
   } else {
-    res.writeHead(404);
-    console.log('404');
-    res.end(); 
+    const message = { message: 'this request does not exist' };
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(message, null, 2));
   }
-};
+});
 
 exports.handler = requestHandler;

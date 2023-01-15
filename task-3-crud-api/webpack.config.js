a78c6df0a3429754fb6c07f7acbfc164d69f1436
Natalia <sh.nat.vlad@gmail.com>
2023-01-15 17:32:44 +0600

    feat: add start:dev and start:prod

---
+++ task-3-crud-api/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
